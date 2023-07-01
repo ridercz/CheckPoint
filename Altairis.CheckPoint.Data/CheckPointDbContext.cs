d8cfe85a4530cadee34fd058a151369de6c0f5a3
Michal Altair Valášek <github@rider.cz>
2023-07-02 01:22:19 +0200

    Initial commit.

---
+++ Altairis.CheckPoint.Data/CheckPointDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Altairis.CheckPoint.Data;
+
+public class CheckPointDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid> {
+
+    public CheckPointDbContext(DbContextOptions<CheckPointDbContext> options) : base(options) { }
+
+}
---
+++ Altairis.CheckPoint.Web/Program.cs
@@ -0,0 +1,16 @@
+using Altairis.CheckPoint.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add CheckPointDbContext
+builder.Services.AddDbContext<CheckPointDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+var app = builder.Build();
+
+
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
